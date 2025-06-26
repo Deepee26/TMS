a2a127ab4c7df7a28c544ca5fed2df0457480131
Dechen Peldon <dechenpelden05@gmail.com>
2025-06-26 10:26:51 +0600

    initial commit

--- config/db.js
+++ config/db.js
@@ -7,11 +7,13 @@ const pgp = pgpInit();
 
 const db = pgp({
     host: 'localhost',
-    port: process.env.DB_PORT,
+    port: 5432, //default postgresSQL port
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    ssl: false,
+    ssl: {
+        rejectUnauthorized: false //use this only if you are sure about the security of your
+    },
 })
 
 
